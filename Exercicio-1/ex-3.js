126e172616b6f1fff1726dc0727692d7b79b60c9
Dennis1334 <90472903+Dennis1334@users.noreply.github.com>
2024-10-29 21:23:37 -0300

    Add files via upload
    exercícios de 1 a 6

---
+++ Exercicio-1/ex-3.js
@@ -0,0 +1,4 @@
+var DigiteNumeroA = parseInt(prompt("Digite o Numero"))
+var DigiteNumeroB = parseInt(prompt("Digite o Numero"))
+var res = DigiteNumeroA + DigiteNumeroB
+console.log("O valor da soma: ", res)
